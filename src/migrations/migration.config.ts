14e543692e471395125abe7c6c36afd786b12ce7
bellanzo <abimbolaomisakin678@gmail.com>
2026-01-05 17:41:12 +0100

    fix(migration file): error fix

--- src/migrations/migration.config.ts
+++ src/migrations/migration.config.ts
@@ -16,7 +16,6 @@ const dataSource = new DataSource({
   database: configService.get('DATABASE_NAME'),
   entities: ['dist/**/entities/*.entity{.ts,.js}'],
   migrations: ['dist/db/migrations/*.js'],
-  synchronize: true,
   ssl: isProduction
     ? { rejectUnauthorized: false } // cloud DBs
     : false, // local DB
